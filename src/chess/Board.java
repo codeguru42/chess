31a151f2bfe52aaaf6673b384c1dfe8b4781a5c9
codeguru <codeguru@users.sourceforge.net>
2013-06-16 10:34:04 -0600

    Chess: Move all classes to chess package

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -5,6 +5,8 @@
  * To Public License, Version 2, as published by Sam Hocevar. See
  * http://sam.zoy.org/wtfpl/COPYING for more details.
  */
+package chess;
+
 import java.util.ArrayList;
 import java.util.List;
 
