62b63c7b81d53e60ac98fb230ce7584fc902cc96
codeguru <codeguru@users.sourceforge.net>
2013-06-16 10:40:49 -0600

    AllTests: Create JUnit test suite to run all test classes

---
+++ test/chess/AllTests.java
@@ -0,0 +1,19 @@
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
+package chess;
+
+import chess.pieces.PawnTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BoardTest.class, PawnTest.class })
+public class AllTests {
+
+}
